d06865c04c6619f01fe95f131dc2046cfa788987
natipoi <yota@natipoi.com>
2020-07-20 07:42:18 +0900

    m

--- docs/_plugins/backend.rb
+++ docs/_plugins/backend.rb
@@ -4,11 +4,11 @@ require 'Base64'
 require 'httpclient'
 
 
-app_token = "EAAEIkS05XYsBAJ7ijUw4fu9HeWhWxe9o62QLqFXRxmXDKYK1TvANVRUPGBjqiJpjcFdVRrzJcuGz8jgClZAHL5uDwIDsVzVzYGMnavPbMPtW3uMWL8eFdVJl8fmEFw9NWGfhyEWRNQae04SrdXHacfQzeblSlz7yax5RuZBAZDZD"
-business_id = "111946820571734"
+app_token = "EAAC3KYGFzNABAKNZC6fDxyiZBnsbfBBf77IVZBnh7YURYC7Nf76wLlDMyDfBakaVTCBu3vkh196ZAblaPVrxUXcqyZBZB1ZBuN7kmzWiQ2Nc1ia6cdJ7kynLZC1duVA4HyX69plozqHUkrIZBDfZBlqcZB9PsyqoTUnAMvMA0tSjHZAusQZDZD"
+business_id = "17841438602473274"
 secret = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), "2f5751058c8396f6559a3780cbe5fda4", app_token)
 
-url = 'https://graph.facebook.com/v7.0/' + business_id + '?fields=photos&access_token=' + app_token + '&appsecret_proof=' + secret
+url = 'https://graph.facebook.com/v7.0/' + business_id + '?fields=photos&access_token=' + app_token
 # url = "https://graph.facebook.com/v7.0/" + friend.uid + "/"
 
 
